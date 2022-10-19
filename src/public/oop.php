5a52a6cfa3d786ffdfc3c797462d25eb90bb6713
aamuk <aam1989@protonmail.com>
2022-10-19 21:35:05 +0100

    p 31

--- src/public/oop.php
+++ src/public/oop.php
@@ -1,18 +1,36 @@
 <?php
 
-echo "<h1><u>OOP</u></h1><p><i>Page <strong>25</strong></i></p><hr>";
+echo "<h1><u>OOP</u></h1><p><i>Page <strong>31</strong></i></p><hr>";
 
-class ShopProduct {
-    public $title = "default product";
-    public $producerMainName = "main name";
-    public $producerFirstName = "first name";
+class ShopProduct
+{
+    public $title;
+    public $producerMainName;
+    public $producerFirstName;
     public $price = 0;
+
+    public function __construct(
+        $title,
+        $firstName,
+        $mainName,
+        $price
+    )
+    {
+        $this->title = $title;
+        $this->producerFirstName = $firstName;
+        $this->producerMainName = $mainName;
+        $this->price = $price;
+    }
+
+    public function getProducer()
+    {
+        return $this->producerFirstName . " " . $this->producerMainName;
+    }
 }
 
-$product1 = new ShopProduct();
-$product2 = new ShopProduct();
-
-$product1->title = "lol";
-
-echo $product1->title;
-
+$product1 = new ShopProduct(
+    "Banana",
+    "Smith",
+    "Dave",
+    12
+    );
